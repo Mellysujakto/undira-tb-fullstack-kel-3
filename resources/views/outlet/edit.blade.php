076fbbb0afde7fb750ce34f3ad803c07d334f59e
Laraspnd3 <133668343+Laraspnd3@users.noreply.github.com>
2023-07-01 22:35:28 +0700

    enable auth in each page by user data handling (#2)

--- resources/views/outlet/edit.blade.php
+++ resources/views/outlet/edit.blade.php
@@ -1,53 +1,62 @@
-@extends('layouts.header')
+@extends('layouts.app')
+
 
 @section('title','Edit Data Outlet')
 
-@section('konten')
+
+@section('content')
 <div class="p-5 mb-3">
-    <div class="container">
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div><br />
-        @endif
-        @if (\Session::has('success'))
-        <div class="alert alert-success">
-            <p>{{ \Session::get('success') }}</p>
-        </div><br />
-        @endif
+<div class="container">
+@if ($errors->any())
+<div class="alert alert-danger">
+<ul>
+@foreach ($errors->all() as $error)
+<li>{{ $error }}</li>
+@endforeach
+</ul>
+</div><br />
+@endif
+@if (\Session::has('success'))
+<div class="alert alert-success">
+<p>{{ \Session::get('success') }}</p>
+</div><br />
+@endif
 
-        <form action="{{action('OutletController@update',$id)}}" method="post">
-            {{csrf_field()}}
-            <input name="_method" type="hidden" value="PATCH">
-            <div class="mb-3 row">
-                <label for="nama_outlet" class="col-2 col-form-label">Nama Outlet</label>
-                <div class="col-10">
-                    <input type="text" class="form-control" name="nama_outlet" id="nama_outlet" value="{{$product->nama_outlet}}">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="lokasi_outlet" class="col-2 col-form-label">Lokasi</label>
-                <div class="col-10">
-                    <input type="text" class="form-control" name="lokasi_outlet" id="lokasi_outlet" value="{{$product->lokasi_outlet}}">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="nama_pj" class="col-2 col-form-label">Nama Penanggung Jawab</label>
-                <div class="col-10">
-                    <input type="text" class="form-control" name="nama_pj" id="nama_pj" value="{{$product->nama_pj}}">
-                </div>
-            </div>
 
-            <div class="mb-3 row">
-                <div class="">
-                    <button type="submit" class="btn btn-primary">Update Data</button>
-                </div>
-            </div>
-        </form>
-    </div>
+<form action="{{action('OutletController@update',$id)}}" method="post">
+{{csrf_field()}}
+<input name="_method" type="hidden" value="PATCH">
+<div class="mb-3 row">
+<label for="nama_outlet" class="col-2 col-form-label">Nama Outlet</label>
+<div class="col-10">
+<input type="text" class="form-control" name="nama_outlet" id="nama_outlet" value="{{$product->nama_outlet}}">
+</div>
+</div>
+<div class="mb-3 row">
+<label for="lokasi_outlet" class="col-2 col-form-label">Lokasi</label>
+<div class="col-10">
+<input type="text" class="form-control" name="lokasi_outlet" id="lokasi_outlet" value="{{$product->lokasi_outlet}}">
+</div>
+</div>
+<div class="mb-3 row">
+<label for="nama_pj" class="col-2 col-form-label">Nama Penanggung Jawab</label>
+<div class="col-10">
+<input type="text" class="form-control" name="nama_pj" id="nama_pj" value="{{$product->nama_pj}}">
+</div>
+</div>
+
+
+<div class="mb-3 row">
+<div class="">
+<button type="submit" class="btn btn-primary">Update Data</button>
+</div>
+</div>
+</form>
+</div>
 </div>
 @endsection
+
+
+
+
+
